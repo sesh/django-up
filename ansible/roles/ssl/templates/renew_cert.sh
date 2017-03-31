83f8ec67659874cfc5692be34c162c86fecae20c
Brenton Cleeland <brenton@brntn.me>
2017-03-31 13:41:48 +1100

    Abandon shop if something errors

--- ansible/roles/ssl/templates/renew_cert.sh
+++ ansible/roles/ssl/templates/renew_cert.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 python /var/www/{{ app_name }}/acme-tiny/acme_tiny.py --account-key /var/certs/account.key --csr /var/certs/{{ app_name }}.csr --acme-dir /var/www/challenges/ > /tmp/signed.crt || exit
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
 cat /tmp/signed.crt intermediate.pem > /var/certs/{{ app_name }}.chained.pem
