f4116650e9c24c00c6ee9bcd288a8f36c970e98c
Brenton Cleeland <brenton@brntn.me>
2017-03-31 14:10:16 +1100

    Fix cert renewal

--- ansible/roles/ssl/templates/renew_cert.sh
+++ ansible/roles/ssl/templates/renew_cert.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
-python /var/www/{{ app_name }}/acme-tiny/acme_tiny.py --account-key /var/certs/account.key --csr /var/certs/{{ app_name }}.csr --acme-dir /var/www/challenges/ > /tmp/signed.crt || exit
-wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
-cat /tmp/signed.crt intermediate.pem > /var/certs/{{ app_name }}.chained.pem
-nginx -s reload
+python /var/www/{{ app_name }}/acme-tiny/acme_tiny.py --account-key /var/certs/account.key --csr /var/certs/{{ app_name }}.csr --acme-dir /var/www/challenges/ > /tmp/signed-{{ app_name }}.crt || exit
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate-{{ app_name }}.pem
+cat /tmp/signed-{{ app_name }}.crt intermediate-{{ app_name }}.pem > /var/certs/{{ app_name }}.chained.pem
+systemctl reload nginx
