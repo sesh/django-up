3ed3ebc273df4a677697ddd9d67711822966dfe9
Brenton Cleeland <brenton@brntn.me>
2020-11-19 08:45:12 +1100

    Ubuntu 20.04

--- management/commands/up.py
+++ management/commands/up.py
@@ -19,7 +19,7 @@ from django.utils.crypto import get_random_string
 Deploying Django applications as quickly as you create them
 
 Usage:
-    ./manage.py up <hostname>... [--debug] [--python2]
+    ./manage.py up <hostname>... [--debug]
 
 Your application needs to be configured to use dj_database_url and include your
 target domain in ALLOWED_HOSTS.
@@ -35,10 +35,7 @@ class Command(BaseCommand):
         parser.add_argument('--domain', nargs=1, type=str)
         parser.add_argument('--debug', action='store_true', default=False, dest='debug')
         parser.add_argument('--quick', action='store_true', default=False, dest='quick')
-        parser.add_argument('--ssl', action='store_true', default=False, dest='ssl')
-        parser.add_argument('--python2', action='store_true', default=False, dest='python2')
         parser.add_argument('--verbose', action='store_true', default=False, dest='verbose')
-        parser.add_argument('--dont-force-ssl', action='store_true', default=False, dest='dont_force_ssl')
 
     def handle(self, *args, **options):
         ansible_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'ansible')
@@ -53,7 +50,7 @@ class Command(BaseCommand):
 
         django_environment = {}
         try:
-            with open(os.path.join(settings.BASE_DIR, '.up-env')) as env_file:
+            with open(os.path.join(settings.BASE_DIR, '.env')) as env_file:
                 for line in env_file.readlines():
                     if line and '=' in line and line.strip()[0] not in ['#', ';']:
                         var, val = line.split('=', 2)[:2]
@@ -72,6 +69,8 @@ class Command(BaseCommand):
             '--exclude', '.git',
             '--exclude', '*.sqlite3',
             '--exclude', '__pycache__',
+            '--exclude', 'bubble-images/*.png',
+            '--exclude', '*.log',
             '--exclude', '{}.tar'.format(app_name),
             '--dereference',
             '-cf', app_tar.name, '.'
@@ -115,13 +114,13 @@ class Command(BaseCommand):
                 'domain_names': ' '.join(domains),
                 'gunicorn_port': getattr(settings, 'GUNICORN_PORT', '9000'),
                 'app_tar': app_tar.name,
-                'python_version': getattr(settings, 'UP_PYTHON_VERSION', 'python3.6'),
+                'python_version': getattr(settings, 'UP_PYTHON_VERSION', 'python3.8'),
                 'db_password': db_pass,
                 'django_debug': 'yes' if options['debug'] else 'no',
                 'django_environment': django_environment,
                 'csr_domains': ','.join(['DNS:{}'.format(domain) for domain in domains]),
                 'extra_app_dirs': getattr(settings, 'UP_DIRS', []),
-                'force_ssl': 'no' if options['dont_force_ssl'] else 'yes',
+                'force_ssl': 'yes',
                 'ssl_cron_day_of_week': random.randint(0, 6),
             },
             'roles': [
@@ -138,7 +137,7 @@ class Command(BaseCommand):
         }]
 
         if hasattr(settings, 'UP_EXTRAS'):
-            # available extras: redis, memcached, phantomjs, kronos
+            # available extras: redis, memcached, kronos
             yam[0]['roles'].extend(getattr(settings, 'UP_EXTRAS'))
             print(yam[0]['roles'])
 
@@ -164,9 +163,6 @@ class Command(BaseCommand):
         extra = []
         tags = []
 
-        if options['ssl']:
-            tags.append('ssl')
-
         if options['quick']:
             tags.append('deploy')
 
@@ -177,16 +173,6 @@ class Command(BaseCommand):
         if options['verbose']:
             extra.append('-vvvv')
 
-        # first execute just the `init` tag to ensure Python 2.7 is available (only if not in quick mode)
-        if not options['quick']:
-            yam[0]['gather_facts'] = 'no'
-            app_yml_no_facts = open(os.path.join(up_dir, '{}-nofacts.yml'.format(app_name)), 'w')
-            yaml.dump(yam, app_yml_no_facts)
-            init_command = ['ansible-playbook', '-i', os.path.join(up_dir, 'hosts')]
-            init_command.extend(['-t', 'init'])
-            init_command.extend([os.path.join(up_dir, '{}-nofacts.yml'.format(app_name))])
-            subprocess.call(init_command)
-
         # build the ansible command
         command = ['ansible-playbook', '-i', os.path.join(up_dir, 'hosts')]
         command.extend(extra)
@@ -194,14 +180,3 @@ class Command(BaseCommand):
 
         # execute ansible
         return_code = subprocess.call(command)
-
-        # mark as released on opbeat!
-        if hasattr(settings, 'OPBEAT') and return_code == 0:
-            op_id = settings.OPBEAT['ORGANIZATION_ID']
-            app_id = settings.OPBEAT['APP_ID']
-            rev = subprocess.getoutput("git log -n 1 --pretty=format:%H")
-
-            requests.post('https://intake.opbeat.com/api/v1/organizations/{}/apps/{}/releases/'.format(op_id, app_id), {
-                'rev': rev,
-                'status': 'completed',
-            }, headers={'Authorization': 'Bearer {}'.format(settings.OPBEAT['SECRET_TOKEN'])})
